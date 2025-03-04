a7eb524d17ba059727f7bc175672d81946669118
abhireddy20 <thadur@wisc.edu>
2025-03-04 12:33:31 +0530

    Updated mean.sh

--- mean.sh
+++ mean.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+if [ $# -lt 1 ]; then
+    echo "usage: ./mean.sh <column> [file.csv]" 1>&2
+    exit 1
+fi
+
+column=$1
+file=${2:-/dev/stdin} 
+
+awk -F',' -v col="$column" '
+    NR > 1 { sum += $col; count++ }
+    END { if (count > 0) print sum / count; else print "Error: No data found in column" 1>&2 }
+' "$file"
