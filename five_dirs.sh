ca00e5c54d94b03a37fa39cdba65e3b9f3c61088
Connor Herbert <cwherbert@wisc.edu>
2025-03-04 08:46:25 -0600

    Modified five_dirs.sh

--- five_dirs.sh
+++ five_dirs.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
+mkdir -p five
 
+for i in {1..5}; do
+    dir="five/dir$i"
+    mkdir -p "$dir"
+
+    for j in {1..4}; do
+	file="$dir/file$j"
+	yes "$j" | head -n "$j" > "$file"
+    done
+done
+
+    
