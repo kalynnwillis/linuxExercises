7baee7ca306e2c12acdf9e64fe2f22fc8b9df209
Kalynn Willis <khwillis@wisc.edu>
2025-03-04 15:42:29 -0600

    Created script that removes files larger than given size

--- rm_n.sh
+++ rm_n.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+
+if [[ $# -ne 2 ]]; then
+    echo "Usage: $0 <dir> <n>" 1>&2
+    exit 1
+fi
+
+
+dir=$1
+n=$2
+
+
+find "$dir" -type f -size +"$n"c -exec rm {} \;
+
+echo "Removed all files in '$dir' larger than $n bytes."
